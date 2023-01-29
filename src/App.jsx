0f1953872c08da9c2ef70734dfbdacdf374d0657
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-01-30 00:11:37 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,55 @@
 import { Route, Routes } from 'react-router-dom';
 // import { lazy } from 'react';
 import { Layout } from './components/Layout/Layout';
+
+import { Home } from './pages/Home/Home';
+import { Movies } from './pages/Movies/Movies';
+import { MovieCard } from './pages/MovieCard/MovieCard';
+import { Cast } from './components/Cast/Cast';
+import { Reviews } from './components/Reviews/Reviews';
+import { Error } from './components/Error/Error';
+
 /*
 /**|======================================
 /**| Imported Components (NOT AS DEFAULT FIX)
 /**|======================================
 */
-const Home = () =>
-  import('./pages/Home/Home').then(module => ({
-    ...module,
-    default: module.Home,
-  }));
-const Movies = () =>
-  import('./pages/Movies/Movies').then(module => ({
-    ...module,
-    default: module.Movies,
-  }));
-const MovieCard = () =>
-  import('./pages/MovieCard/MovieCard').then(module => ({
-    ...module,
-    default: module.MovieCard,
-  }));
-const Cast = () =>
-  import('./components/Cast/Cast').then(module => ({
-    ...module,
-    default: module.Cast,
-  }));
-const Reviews = () =>
-  import('./components/Reviews/Reviews').then(module => ({
-    ...module,
-    default: module.Reviews,
-  }));
-const Error = () =>
-  import('./components/Error/Error').then(module => ({
-    ...module,
-    default: module.Error,
-  }));
+// const Home = lazy(() =>
+//   import('./pages/Home/Home').then(module => ({
+//     ...module,
+//     default: module.Home,
+//   }))
+// );
+// const Movies = lazy(() =>
+//   import('./pages/Movies/Movies').then(module => ({
+//     ...module,
+//     default: module.Movies,
+//   }))
+// );
+// const MovieCard = lazy(() =>
+//   import('./pages/MovieCard/MovieCard').then(module => ({
+//     ...module,
+//     default: module.MovieCard,
+//   }))
+// );
+// const Cast = lazy(() =>
+//   import('./components/Cast/Cast').then(module => ({
+//     ...module,
+//     default: module.Cast,
+//   }))
+// );
+// const Reviews = lazy(() =>
+//   import('./components/Reviews/Reviews').then(module => ({
+//     ...module,
+//     default: module.Reviews,
+//   }))
+// );
+// const Error = lazy(() =>
+//   import('./components/Error/Error').then(module => ({
+//     ...module,
+//     default: module.Error,
+//   }))
+// );
 /*
 /**|======================================
 /**| Export App
